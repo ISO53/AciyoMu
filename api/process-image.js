3721d7b2a295016b60a8d68fadcb7b9a5f402a1c
iso53 <102249575+ISO53@users.noreply.github.com>
2024-10-01 16:07:33 +0300

    restricted allowed origins

--- api/process-image.js
+++ api/process-image.js
@@ -14,11 +14,19 @@ const model = genAI.getGenerativeModel({
     model: "gemini-1.5-pro-002",
     systemInstruction: "Never use markdown formatting or any other formatting, return your answer in raw JSON.",
 });
+const allowedOrigins = [
+    "https://aciyo-mu-m3gt.vercel.app",
+    "https://aciyomu-iso53s-projects.vercel.app/",
+    "https://aciyomu-git-master-iso53s-projects.vercel.app/",
+];
 
 app.use(express.json({limit: "50mb"}));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) res.header("Access-Control-Allow-Origin", origin);
+
+    res.header("Access-Control-Allow-Origin", "https://aciyo-mu-m3gt.vercel.app,");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
