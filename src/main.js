ce9f888fe7176ea6802d153e6cb433f226c2828f
iso53 <102249575+ISO53@users.noreply.github.com>
2024-10-01 16:53:44 +0300

    revert the refactoring

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import "./assets/main.css";
+import './assets/main.css'
 
-import {createApp} from "vue";
-import App from "./App.vue";
-import {inject} from "@vercel/analytics/*";
+import { createApp } from 'vue'
+import App from './App.vue'
+import { inject } from '@vercel/analytics/*'
 
-inject();
-createApp(App).mount("#app");
+inject()
+createApp(App).mount('#app')
