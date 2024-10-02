eb9b31582847e2eb9d1f3a94569b15505adbc282
iso53 <102249575+ISO53@users.noreply.github.com>
2024-10-02 22:31:42 +0300

    reformatting

--- src/components/OkeyPiece.vue
+++ src/components/OkeyPiece.vue
@@ -16,31 +16,33 @@ export default {
 </script>
 
 <style scoped>
-    .okey-piece {
-        width: 40px;
-        height: 60px;
-        background-color: burlywood;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-evenly;
-        border-radius: 5px;
-        margin: 0 2px 2px 0;
-    }
+.okey-piece {
+    width: 40px;
+    height: 60px;
+    background-color: burlywood;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    border-radius: 5px;
+    margin: 0 2px 2px 0;
+}
 
-    .number {
-        font-size: 1.1rem;
-        font-weight: 900;
-        text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.4);
-        margin: 0;
-        line-height: 1.1rem;
-    }
+.number {
+    font-size: 1.1rem;
+    font-weight: 900;
+    text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.4);
+    margin: 0;
+    line-height: 1.1rem;
+}
 
-    .color {
-        width: 16px;
-        height: 16px;
-        border-radius: 20px;
-        box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.4);
-        margin: 0;
-    }
+.color {
+    width: 16px;
+    height: 16px;
+    border-radius: 20px;
+    box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.4);
+    margin: 0;
+}
+
+
 </style>
